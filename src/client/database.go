0677cb9c5ae9969f6427430f0d693d10282b2752
Jaeyoung Lee <jaeyoung@monodiary.net>
2016-03-01 19:20:00 +0900

    Merge database actions to cli tools

---
+++ src/client/database.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"common"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		helpMessage()
+	}
+
+	var tables = []interface{}{
+		&common.Article{},
+		&common.CrawlLog{}}
+
+	switch os.Args[1] {
+	case "create_all":
+		db := configure()
+		db.CreateTable(tables...)
+	case "drop_all":
+		db := configure()
+		db.DropTable(tables...)
+	default:
+		helpMessage()
+	}
+}
+
+func helpMessage() {
+	fmt.Println("./database [create_all, drop_all]")
+	os.Exit(1)
+}
+
+func configure() gorm.DB {
+	common.InitConfig()
+	config := common.Config
+	dbURI := common.BuildURIFrom(&config.DB)
+	db, _ := gorm.Open("postgres", dbURI)
+	db.DB()
+	db.LogMode(true)
+
+	return db
+}
