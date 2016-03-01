3b92796c9805c4fc316b7a420f926751e6f714f5
Jaeyoung Lee <jaeyoung@monodiary.net>
2016-03-01 18:24:37 +0900

    learning go package structure

---
+++ src/client/bot.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	// "encoding/json"
+	"common"
+	"fmt"
+	irc "github.com/fluffle/goirc/client"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	// "net/http"
+	// "time"
+)
+
+func main() {
+	common.InitConfig()
+	config := common.Config
+
+	// Configure Database
+	dbURI := common.BuildURIFrom(&config.DB)
+	db, dbErr := gorm.Open("postgres", dbURI)
+	if dbErr != nil {
+		fmt.Printf("Database error: %s\n", dbErr.Error())
+	}
+
+	// Configure IRC
+	cfg := irc.NewConfig(config.IRC.Nick)
+	cfg.Server = config.IRC.Server
+	cfg.NewNick = func(n string) string { return config.IRC.NickAlternative }
+	c := irc.Client(cfg)
+	ircQuit := make(chan struct{})
+
+	// Handler for IRC actions
+	c.HandleFunc(irc.CONNECTED, func(conn *irc.Conn, line *irc.Line) { conn.Join(config.IRC.Channel) })
+	c.HandleFunc(irc.DISCONNECTED, func(conn *irc.Conn, line *irc.Line) { ircQuit <- struct{}{} })
+
+	// IRC Connect!
+	if ircErr := c.Connect(); ircErr != nil {
+		fmt.Printf("Connection error: %s\n", ircErr.Error())
+	}
+
+	// a := common.GetKpopNews()
+	// fmt.Println(a)
+
+	<-ircQuit
+}
---
+++ src/common/config.go
@@ -0,0 +1,52 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type DBConfigType struct {
+	Host     string `json:"host"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Port     string `json:"port"`
+	Name     string `json:"name"`
+}
+
+type IRCConfigType struct {
+	Nick            string `json:"nick"`
+	NickAlternative string `json:"nickAlternative"`
+	Server          string `json:"server"`
+	Channel         string `json:"channel"`
+}
+
+type ConfigType struct {
+	DB  DBConfigType  `json:"db"`
+	IRC IRCConfigType `json:"irc"`
+}
+
+var Config ConfigType
+
+func InitConfig() error {
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	err = json.Unmarshal(data, &Config)
+	if err != nil {
+		log.Println(err)
+	}
+	return err
+}
+
+func BuildURIFrom(db *DBConfigType) string {
+	config := *db
+	return fmt.Sprintf(
+		"postgresql://%s:%s@%s:%s/%s",
+		config.User, config.Password,
+		config.Host, config.Port,
+		config.Name)
+}
